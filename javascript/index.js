48319fd4896dd4a87a5b0856c03dc195e15dac06
laroca <laroca73@gmail.com>
2022-07-04 22:08:56 +0200

    Add Iteration 1

--- javascript/index.js
+++ javascript/index.js
@@ -28,7 +28,25 @@
 
 
 // Iteration 1 - using callbacks
-// ...
+// Callbacks in sequence
+getInstruction("mashedPotatoes", 0, (step1) => {
+    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
+    getInstruction("mashedPotatoes", 1, (step2) => {
+        document.querySelector("#mashedPotatoes").innerHTML += `<li>${step2}</li>`;
+        getInstruction("mashedPotatoes", 2, (step3) => {
+            document.querySelector("#mashedPotatoes").innerHTML += `<li>${step3}</li>`;
+            getInstruction("mashedPotatoes", 3, (step4) => {
+                document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`;
+                getInstruction("mashedPotatoes", 4, (step5) => {
+                  document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
+                  document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
+                  document.querySelector("#mashedPotatoes").innerHTML += `<li>Mashed Potatoes are ready!</li>`;
+                }, (error) => console.log(error));
+            },(error) => console.log(error));
+        },(error) => console.log(error));
+    },(error) => console.log(error));
+},(error) => console.log(error));
+
 
 // Iteration 2 - using promises
 // ...
